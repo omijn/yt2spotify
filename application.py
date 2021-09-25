15b8fe9040f57f413968b20b9dafca1e35741b88
omijn <13493921+omijn@users.noreply.github.com>
2021-09-25 09:08:25 -0700

    revive project

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from flask import Flask, request, jsonify, Response, render_template
 from urllib.parse import unquote
 
+
 def read_spotify_config(config_path):
     config = configparser.ConfigParser()
     config.read(config_path)
@@ -16,38 +17,45 @@ def read_spotify_config(config_path):
 
     return client_id, client_secret
 
+
 application = Flask(__name__)
 ytvideopattern = re.compile(r'(?<=v=)[-\w]+')
 yt = YTMusic()
-client_id, client_secret = read_spotify_config("config.ini")
+client_id, client_secret = read_spotify_config('config.ini')
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
 
 def get_spotify_client(client_id, client_secret):
     pass
 
+
 def get_yt_client():
     pass
+
 
 @application.route('/')
 def index():
     return application.send_static_file('index.html')
 
+
 @application.route('/y2s')
 def yt2spotify():
     url = request.args.get('url')
     if url == "" or url is None:
-        return Response("no URL parameter in request", status=400)     # TODO: look into custom exception handlers
+        return Response("no URL parameter in request", status=400)  # TODO: look into custom exception handlers
 
     url = unquote(url)
 
     videos = ytvideopattern.findall(url)
     if len(videos) == 0:
-        return Response("Invalid YouTube Music URL", status=400)   # TODO: mimetype=application/json? 
+        return Response("Invalid YouTube Music URL", status=400)  # TODO: mimetype=application/json?
 
     video_id = videos[0]
     song = yt.get_song(videoId=video_id)
-    
-    spotify_search_query = f"{song['title']} {song['artists'][0]}"
+
+    song_title = song['videoDetails']['title']
+    song_artist = song['videoDetails']['author'].removesuffix(" - Topic")
+    spotify_search_query = f"{song_title} {song_artist}"
     results = sp.search(spotify_search_query, limit=5, type="track")
 
     res_urls = {'results': []}
@@ -56,8 +64,10 @@ def yt2spotify():
 
     return jsonify(res_urls)
 
+
 def spotify2yt(sp, yt, url):
     raise NotImplementedError
 
+
 if __name__ == "__main__":
-    application.run()
+    application.run(host='0.0.0.0', port=5000)
